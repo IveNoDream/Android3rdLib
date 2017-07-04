126ae6d83f3a0009bee26501187f6e2a23a51f81
wangfeng <wangfeng@localhost.com>
2017-07-04 14:48:34 +0800

    Structure

---
+++ Android3rdLib/app/src/main/java/com/wafe/android3rdlib/custom/TestActivity.java
@@ -0,0 +1,10 @@
+package com.wafe.android3rdlib.custom;
+
+import android.app.Activity;
+
+/**
+ * Created by root on 7/4/17.
+ */
+
+public class TestActivity extends Activity {
+}
---
+++ Android3rdLib/app/src/main/java/com/wafe/android3rdlib/frame/TestActivity.java
@@ -0,0 +1,10 @@
+package com.wafe.android3rdlib.frame;
+
+import android.app.Activity;
+
+/**
+ * Created by root on 7/4/17.
+ */
+
+public class TestActivity extends Activity {
+}
