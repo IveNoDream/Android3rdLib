aeb561f29aff3ba65a1c51fbadfcd93cbb5c6c92
wangfeng <wangfeng@localhost.com>
2017-08-08 15:35:35 +0800

    单位转换类

---
+++ Android3rdLib/app/src/main/java/com/wafe/android3rdlib/util/DisplayUtil.java
@@ -0,0 +1,55 @@
+package com.wafe.android3rdlib.util;
+
+import android.content.Context;
+
+/**
+ * dp/sp转换px工具类
+ * Created by root on 8/8/17.
+ */
+
+public class DisplayUtil {
+
+    /**
+     * px value to dip/dp value, keep the same size
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2dop(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue/scale + 0.5f);
+    }
+
+    /**
+     * dip/dp to px
+     * @param context
+     * @param dipValue
+     * @return
+     */
+    public static int dip2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+
+    /**
+     * px to sp ,keep text the same size
+     * @param context
+     * @param pxValue
+     * @return
+     */
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    /**
+     * sp to px
+     * @param context
+     * @param spValue
+     * @return
+     */
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return  (int) (spValue * fontScale + 0.5f);
+    }
+}
